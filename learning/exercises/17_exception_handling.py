02cd1cf437441c6060b50161e64aa91eba0d2b8a
Elkin Jaramillo <ejaram3@outlook.com>
2025-08-24 16:37:21 -0500

    Delete learning/exercises directory

--- learning/exercises/17_exception_handling.py
+++
@@ -1,9 +0,0 @@
-# Desempaqueta los primeros cinco países y almacéralos en una variable nordic_countryies, almacena Estonia y Rusia en es, y ru respectivamente.
-
-names = ['Finland', 'Sweden', 'Norway',
-         'Denmark', 'Iceland', 'Estonia', 'Russia']
-
-*nordic_countries, es, ru = names
-print(nordic_countries)
-print(es)
-print(ru)
